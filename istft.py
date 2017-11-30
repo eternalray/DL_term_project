f9e9c833711e7c6b3848efdbee990495c68f6f2d
eternalray <eternalray123@gmail.com>
2017-12-01 01:00:45 +0900

    initial commit

---
+++ istft.py
@@ -0,0 +1,34 @@
+import argparse
+import numpy as np
+import os.path
+from scipy.io import wavfile
+from scipy.signal import istft
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filepath", help="input directory where csv files reside")
+args = parser.parse_args()
+
+directory_path = args.filepath
+loc = directory_path.rfind('/', 0, len(directory_path)-1)
+
+prefix = directory_path[loc+1:]
+file_name_idx = 1
+ext = ".csv"
+
+preblock = directory_path + "/" + prefix + "_"
+file_name = preblock + str(file_name_idx) + ext
+wav_data = []
+
+print("istft started for " + prefix)
+while os.path.isfile(file_name):
+	data = np.loadtxt(file_name, dtype=np.complex64)
+	print(data)
+	break
+	time, wav_data_tmp = istft(data, 44100)
+	wav_data = wav_data + wav_data_tmp.tolist()
+	file_name_idx += 5
+	file_name = preblock + str(file_name_idx) + ext
+
+print("istft done")
+wav_data_array = np.asarray(wav_data, )
+wavfile.write(directory_path + "/i_" + prefix + ".wav", 44100, wav_data_array)
---
+++ stft.py
@@ -0,0 +1,52 @@
+import argparse
+import os
+import numpy as np
+from scipy.io import wavfile
+from scipy.signal import stft
+from matplotlib import pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filepath", help="input path of wav file or directory")
+args = parser.parse_args()
+
+file_name = args.filepath
+
+rate, data = wavfile.read(file_name)
+
+
+left = data[:, 0]
+width = 5 * rate
+start = width
+end = start + width
+loc = file_name.rfind('/')
+directory_path = file_name[:loc+1]
+
+prefix = "spectro_" + file_name[loc+1:-4]
+
+file_name_idx = 1
+if not os.path.exists(directory_path + prefix):
+	os.makedirs(directory_path + prefix)
+print("stft started for " + file_name)
+while end < left.shape[0]:
+	spectro_file_path = directory_path + prefix + "/" + prefix + "_" + str(file_name_idx) + ".csv"
+	window = left[start:end]
+	freq, time, spectro = stft(window, rate)
+
+	start += rate
+	end += rate
+	np.savetxt(spectro_file_path, spectro, delimiter=',')
+	file_name_idx += 1
+
+print("stft done")
+
+	
+
+'''
+plt.pcolormesh(time, freq, spectro)
+
+plt.ylabel("Frequency [Hz]")
+plt.xlabel("time [sec]")
+plt.ylim(0,5000)
+#plt.xlim(10,20)
+plt.show()
+'''
